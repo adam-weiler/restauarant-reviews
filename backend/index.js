093f56aa4afbeeb3ed5cf18d02c22522b94cac4f
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-01 21:55:10 -0500

    Server now starting.

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,16 @@ const port = process.env.PORT || 8000;  // Use port from .env file, or 8000.
 MongoClient.connect(    // Connect to database.
     process.env.RESTREVIEWS_DB_URI, // The environment variable with username & password for MongoDB.
     {
-        poolSize: 50,      // Only 50 ppl can connect at once.
+        // poolSize: 50,      // Only 50 ppl can connect at once.
         wtimeout: 2500,      // After 2500 miliseconds the request times out.
-        useNewUrlParse: true    // They recently changed MongoDB driver, and put the new connection parser behind this flag.
+        // useNewUrlParse: true    // They recently changed MongoDB driver, and put the new connection parser behind this flag.
     })
     .catch(err => {      // If error, log to console and exit the process.
         console.error(err.stack);  
         process.exit(1);
     })
     .then(async client => {     // Succesful connection to database.
-        app.listen(port, () => {
+        app.listen(port, () => {    // Starts our web server.
             console.log(`Listening on port ${port}`);
         })
     }
