d62466ad18e680146e53996c1aa93deeaa0a97e8
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-01 21:27:05 -0500

    Creating index and server files. Adding package.json files. Ignoring node-modules now.

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import app from "./server.js";
+import mongodb from "mongodb";  // Used to access mongodb.
+import dotenv from "dotenv";    // Used to access environment variables.
+dotenv.config();    // Load in environment variables.
+const MongoClient = mongodb.MongoClient;    // Access our Mongo client.
+
+const port = process.env.PORT || 8000;  // Use port from .env file, or 8000.
+
+MongoClient.connect(    // Connect to database.
+    process.env.RESTREVIEWS_DB_URI, // The environment variable with username & password for MongoDB.
+    {
+        poolSize: 50,      // Only 50 ppl can connect at once.
+        wtimeout: 2500,      // After 2500 miliseconds the request times out.
+        useNewUrlParse: true    // They recently changed MongoDB driver, and put the new connection parser behind this flag.
+    })
+    .catch(err => {      // If error, log to console and exit the process.
+        console.error(err.stack);  
+        process.exit(1);
+    })
+    .then(async client => {     // Succesful connection to database.
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        })
+    }
+);
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import restaurants from "./api/restaurants.route.js";
+
+const app = express();
+
+app.use(cors());    // Apply our middleware, CORS module.
+app.use(express.json());    // Our server can accept JSON in the body of a request. Previously required body-parser, but express can do this now.
+
+app.use("/api/v1/restaurants", restaurants);
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));    // A route that doesn't exist in our route file.
+
+export default app; // Export this file as a module.
